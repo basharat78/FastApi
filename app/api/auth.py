25d0129b139d0d837775a3eaf26ae8930cea8991
mac <mac@macs-MacBook-Pro.local>
2026-07-14 21:07:20 +0500

    dumpy data and validatin basics

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter 
 from app.schemas.auth_schma import LoginRequest
+from app.database.user import users
+
 router = APIRouter(
     prefix="/auth",
     tags=["auth"])
 @router.post("/login")
 def login(login_request: LoginRequest):
-    return {
-    "message": "Login request received",
-    "login": login_request.login
-}
+  for user in users:
+    if user["login"] == login_request.login and user["password"] == login_request.password:
+      return {"message": "Login successful"}
+  return {"message": "Invalid login or password"}
---
+++ app/database/user.py
@@ -0,0 +1,12 @@
+users = [
+    {
+        "id": 1,
+        "login": "admin@example.com",
+        "password": "123456"
+    },
+    {
+        "id": 2,
+        "login": "user@example.com",
+        "password": "abcdef"
+    }
+]
